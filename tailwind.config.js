a1782a82d67dd1a5fc767210a1ad2a728885b18f
unknown <khaledsaoud237@gmail.com>
2025-01-20 19:32:31 +0100

    positivus

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        sltygreen: "#B9FF66",
+        saltygreen: "#b9ff66",
         darkblue: "#191A23",
         cream: "#fffdf0",
       },
