8dcafbe755b54abff7c8414f1d2e915d1c7c92d1
unknown <khaledsaoud237@gmail.com>
2025-01-20 17:15:28 +0100

    positivus

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        sltygreen: "#B9FF66",
+        darkblue: "#191A23",
+        cream: "#fffdf0",
+      },
+      fontFamily: {
+        trykker: ["Trykker", "serif"],
+      },
+    },
+  },
+};
